6ef934fcf0616143e002f93d003851999bada0b3
Maurice B <maurice.beilfuss@gmx.de>
2016-06-24 13:37:06 +0200

    dgsgsd

---
+++ src/main/java/com/mycompany/WicketApplication.java
@@ -0,0 +1,43 @@
+package com.mycompany;
+
+import com.mycompany.HomePage;
+import com.mycompany.SuccessPage;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application.
+ * If you want to run this application without deploying, run the Start class.
+ * 
+ * @see com.mycompany.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{
+	/**
+     * @return 
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<? extends WebPage> getHomePage()
+	{
+		return HomePage.class;
+	}
+
+	public Class<? extends WebPage> getSuccessPage()
+	{
+		return SuccessPage.class;
+	}
+        
+        
+        
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	/*@Override
+	public void init()
+	{
+		super.init();
+
+		// add your configuration here
+	}*/
+}
