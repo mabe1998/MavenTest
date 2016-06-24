41f8e8f62f8fa619cb219133792eb9149b329aa7
Maurice B <maurice.beilfuss@gmx.de>
2016-06-24 16:21:22 +0200

    Works now with name (but not surname)

--- src/main/java/com/mycompany/HomePage.java
+++ src/main/java/com/mycompany/HomePage.java
@@ -1,47 +1,49 @@
 package com.mycompany;
 
-import com.mycompany.SuccessPage;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.PropertyModel;
+
 
 public class HomePage extends WebPage {
 	
     private static final long serialVersionUID = 1L;
+    
+    private String output = "";
+    
         public HomePage(){
-		add(new Link("id"){
-			@Override
-			public void onClick() {			   
-                            PageParameters pageParameters = new PageParameters();
-                            pageParameters.add("name", "");
-                            pageParameters.add("sirname", "");
-                            setResponsePage(SuccessPage.class);
-			}			
-		});
-                add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-                add(new Label("name"));
-                add(new Label("sirname"));
-	}
-
-//                Form<?> form;
-                
 		
-// TODO Add your page's components here
-//            form = new Form<Void>("userForm") {
+            PropertyModel<String> nameModel = new PropertyModel<>(this, "output");
+            
+            add(new Label("output", nameModel));
+            
+            Form<?> form = new Form("form");
+            form.add(new TextField<>("inputName", nameModel));
+            add(form);
+            
+            add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
+	}
+        
+        public String getOutput()
+        {
+            return output;
+        }
+        
+        public void setOuput(String output)
+        {
+            this.output = output;
+        }
+}
+
+		
+
                 
-                protected void onSubmit() {
-                    
-                    PageParameters pageParameters = new PageParameters();
-                    pageParameters.add("name", "");
-                    pageParameters.add("sirname", "");
-                    setResponsePage(SuccessPage.class, pageParameters);
-                    
-                }
                 
                 
-            };
+                
+         
 
     
 
--- src/main/java/com/mycompany/WicketApplication.java
+++ src/main/java/com/mycompany/WicketApplication.java
@@ -1,16 +1,9 @@
 package com.mycompany;
 
-import com.mycompany.HomePage;
-import com.mycompany.SuccessPage;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
 
-/**
- * Application object for your web application.
- * If you want to run this application without deploying, run the Start class.
- * 
- * @see com.mycompany.Start#main(String[])
- */
+
 public class WicketApplication extends WebApplication
 {
 	/**
@@ -18,17 +11,11 @@ public class WicketApplication extends WebApplication
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
 	@Override
-	public Class<? extends WebPage> getHomePage()
+	public Class< ? extends WebPage> getHomePage()
 	{
 		return HomePage.class;
 	}
 
-	public Class<? extends WebPage> getSuccessPage()
-	{
-		return SuccessPage.class;
-	}
-        
-        
         
 	/**
 	 * @see org.apache.wicket.Application#init()
